f0a814398bf28df3f8e22e65e1aaa98c1ecc986a
Kevin Robinson <krobinson@wi-fi.org>
2014-08-30 05:11:05 +0800

    FT refactor

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.http.request import HttpRequest
 
 from lists.views import home_page
+from django.template.loader import render_to_string
 
 
 class HomePageTest(TestCase):
@@ -18,3 +19,18 @@ class HomePageTest(TestCase):
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.strip().endswith(b'</html>'))
         
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        
+        response = home_page(request)
+        
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'}
+        )
+        self.assertEqual(response.content.decode(), expected_html)
+        
+    
